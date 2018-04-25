6314b8f4cea1c2de0f0ade373637a142db382903
Yehen_veklyn@epam.com <1111111q>
2018-04-26 00:45:25 +0300

    refactor naming

--- src/main/java/bridge2/Main.java
+++ src/main/java/bridge2/Main.java
@@ -7,12 +7,12 @@ public class Main {
 
     public static void main(String [] args){
 
-        Appartment appartmentSwitch = new Appartment();
-        appartmentSwitch.turnOn(new Lamp());
-        appartmentSwitch.turnOn(new TV());
+        Appartment aptSwitch = new Appartment();
+        aptSwitch.turnOn(new Lamp());
+        aptSwitch.turnOn(new TV());
 
-        Building buildingSwitch = new Building();
-        buildingSwitch.turnOff(new Lamp());
-        buildingSwitch.turnOff(new TV());
+        Building bldSwitch = new Building();
+        bldSwitch.turnOff(new Lamp());
+        bldSwitch.turnOff(new TV());
     }
 }
